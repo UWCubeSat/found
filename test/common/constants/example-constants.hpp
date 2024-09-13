188e9e780edf7cadf5a59f8c7c423af06d8ac463
Tri Nguyen <nguy8tri@uw.edu>
2024-09-13 12:15:11 -0700

    Merging from refractor

--- test/common/constants/example-constants.hpp
+++ test/common/constants/example-constants.hpp
@@ -1,7 +1,10 @@
 /**
- * Include all relavant hpp files here where you need access to specific cpp files. The Makefile
- * is configured to use those files despite being in a seperate folder, so it will be recognized
-*/
+ * This is our constants file. We define it as an .hpp file simply
+ * because all our variables are "constants". In practice, you should
+ * aim to define ANY variable or function that you tend to use
+ * repeatedly in a constants file
+ */
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -14,11 +17,18 @@
 #include "test/common/mocks/example-mocks.hpp"
 
 namespace found {
+
 // Array that holds one
 unsigned char arr[1] = {1};
-// Expected result
+
+/**
+ * Expected Results from our Mock
+ */
+
+// Expected Points
 Vec2 expectedPoint1 = {1.0, 2.0};
 Vec2 expectedPoint2 = {3.0, 4.0};
 Points expectedPoints = {expectedPoint1, expectedPoint2};
+// Expected Size of Points
 const long unsigned int expectedSize = 2;  // NOLINT
 }  // namespace found
