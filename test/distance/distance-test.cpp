e3dde535e31828321cbe071c226882d96d011299
tan7271 <christophertan727@gmail.com>
2025-02-20 17:36:01 -0800

    Attempting to write testcase with non-trivial edge points

--- test/distance/distance-test.cpp
+++ test/distance/distance-test.cpp
@@ -53,7 +53,7 @@ std::ostream &operator<<(std::ostream &stream, const Vec3 &vector) {
 
 // Base Case I: The image captured contains an edge centered about the image
 
-TEST(SphericalDistanceDeterminationAlgorithmTest, TestCenteredEarthX) {
+TEST(SphericalDistanceDeterminationAlgorithmTest, TestCenteredEarthX1) {
     // Step I: Pick some distance (km) and a Camera
     decimal x_E = RADIUS_OF_EARTH + 1000;
     int imageWidth = 1024;
@@ -73,6 +73,43 @@ TEST(SphericalDistanceDeterminationAlgorithmTest, TestCenteredEarthX) {
     Vec3 p1 = {static_cast<decimal>(p * cos(alpha)), static_cast<decimal>(p * sin(alpha)), 0};
     Vec3 p2 = {static_cast<decimal>(p * cos(alpha)), static_cast<decimal>(-p * sin(alpha)), 0};
     Vec3 p3 = {static_cast<decimal>(p * cos(alpha)), 0, static_cast<decimal>(p * sin(alpha))};
+
+    // Step III: Use CTS to convert to 2D vectors
+    Points pts = {cam.SpatialToCamera(p1),
+                cam.SpatialToCamera(p2),
+                cam.SpatialToCamera(p3)};
+
+    // Step IV: Run It and Test!
+    SphericalDistanceDeterminationAlgorithm algo =
+        SphericalDistanceDeterminationAlgorithm(RADIUS_OF_EARTH, cam);
+
+    PositionVector actual = algo.Run(pts);
+
+    VECTOR_EQUALS(expected, actual, DEFAULT_TOLERANCE);
+}
+
+TEST(SphericalDistanceDeterminationAlgorithmTest, TestCenteredEarthX2) {
+    // Step I: Pick some distance (km) and a Camera
+    decimal x_E = RADIUS_OF_EARTH + 1000;
+    int imageWidth = 1024;
+    int imageHeight = 1024;
+    Camera cam(0.012, imageWidth, imageHeight);  // Focal length of 12 mm
+    PositionVector expected = {x_E, 0, 0};
+
+    // Step II: Figure out my projection points
+
+    // a) Find the angle
+    decimal alpha = asin(RADIUS_OF_EARTH / x_E);
+
+    // b) Find the distance away from each projection point
+    decimal p = sqrt(x_E * x_E - RADIUS_OF_EARTH * RADIUS_OF_EARTH);
+    decimal centerMag = static_cast<decimal>(p * cos(alpha));
+    decimal projectionRadiusMag = static_cast<decimal>(-p * sin(alpha));
+
+    // c) Use 3 easy projections
+    Vec3 p1 = {centerMag, projectionRadiusMag * cos((decimal) 0.1), projectionRadiusMag * -sin((decimal) 0.1)};
+    Vec3 p2 = {centerMag, projectionRadiusMag, 0};
+    Vec3 p3 = {centerMag, projectionRadiusMag * cos((decimal) 0.1), projectionRadiusMag * sin((decimal) 0.1)};
 
     // Step III: Use CTS to convert to 2D vectors
     Points pts = {cam.SpatialToCamera(p1),
