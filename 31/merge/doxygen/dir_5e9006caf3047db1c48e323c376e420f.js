b831c57fe819c9afcc9cafadc8ee7a0cf4638da2
j4lando <j4lando@users.noreply.github.com>
2025-07-12 09:21:05 +0000

    Deploying to documentation from @ UWCubeSat/found@8e035410cde754beced61bbe641dcceb2f1ba115 🚀

---
+++ 31/merge/doxygen/dir_5e9006caf3047db1c48e323c376e420f.js
@@ -0,0 +1,4 @@
+var dir_5e9006caf3047db1c48e323c376e420f =
+[
+    [ "time.hpp", "time_8hpp_source.html", null ]
+];
---
+++ 31/merge/doxygen/index.js
@@ -0,0 +1,27 @@
+var index =
+[
+    [ "Installation", "index.html#autotoc_md1", [
+      [ "Install Script", "index.html#autotoc_md2", [
+        [ "Prerequisites", "index.html#autotoc_md3", null ],
+        [ "Building FOUND", "index.html#autotoc_md4", null ]
+      ] ],
+      [ "Docker", "index.html#autotoc_md5", [
+        [ "Building FOUND", "index.html#autotoc_md6", null ]
+      ] ],
+      [ "Compilation", "index.html#autotoc_md7", null ]
+    ] ],
+    [ "Usage", "index.html#autotoc_md8", [
+      [ "Example Usage", "index.html#autotoc_md9", null ]
+    ] ],
+    [ "Capabilities", "index.html#autotoc_md10", [
+      [ "Calibration", "index.html#autotoc_md11", null ],
+      [ "Distance Determination", "index.html#autotoc_md12", [
+        [ "Edge Detection", "index.html#autotoc_md13", null ],
+        [ "Distance Determination", "index.html#autotoc_md14", null ],
+        [ "Vector Generation", "index.html#autotoc_md15", null ]
+      ] ],
+      [ "Orbit Determination", "index.html#autotoc_md16", null ]
+    ] ],
+    [ "Tools", "index.html#autotoc_md17", null ],
+    [ "CMake Integration", "index.html#autotoc_md18", null ]
+];
