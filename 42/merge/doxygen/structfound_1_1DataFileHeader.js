4dc1e9b9e29bdfd1f9420c2aa6feef4d61c41b49
j4lando <54454202+j4lando@users.noreply.github.com>
2025-10-04 23:07:07 +0000

    Deploying to documentation from @ UWCubeSat/found@014636ce52740cc42a737c343dc145f6680bc46a 🚀

--- 42/merge/doxygen/structfound_1_1DataFileHeader.js
+++
@@ -1,7 +0,0 @@
-var structfound_1_1DataFileHeader =
-[
-    [ "crc", "structfound_1_1DataFileHeader.html#ac9a8c7d909141180a79bbe7277da5f07", null ],
-    [ "magic", "structfound_1_1DataFileHeader.html#af39bcedbc66cfe3954d9e2399503cad1", null ],
-    [ "num_positions", "structfound_1_1DataFileHeader.html#abd86dada0574499097a5e29f8ff94f67", null ],
-    [ "version", "structfound_1_1DataFileHeader.html#a4195a88cc69f3da902ae753edf8e3d52", null ]
-];
