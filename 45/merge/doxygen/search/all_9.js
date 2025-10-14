7a79697b7f126c7daecb325d5e0d96f2d7db0fb0
j4lando <j4lando@users.noreply.github.com>
2025-10-14 21:00:13 +0000

    Deploying to documentation from @ UWCubeSat/found@7f95629991cb3e5f9d484fa052bdf349231ee91d 🚀

---
+++ 45/merge/doxygen/search/all_9.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['i_0',['i',['../classfound_1_1Quaternion.html#a8e9cc49e2a7ee775910c8a40047e82ac',1,'found::Quaternion']]],
+  ['image_1',['image',['../structfound_1_1Image.html',1,'found::Image'],['../structfound_1_1Image.html#a02d428a46444c8f7001e8916cbed7f49',1,'found::Image::image']]],
+  ['initialcondition_2',['initialCondition',['../structfound_1_1OrbitParams.html#a67613adafcaf9a4507e4637a7e7067c1',1,'found::OrbitParams']]],
+  ['inplanerotation_3',['inPlaneRotation',['../structfound_1_1OrbitParams.html#a82aa7a79b4ace0a4b3f348da96560fcb',1,'found::OrbitParams']]],
+  ['insensor_4',['InSensor',['../classfound_1_1Camera.html#a0de32940821a98fdf6ce10c4898d426c',1,'found::Camera']]],
+  ['install_20script_5',['Setup via Install Script',['../index.html#autotoc_md2',1,'']]],
+  ['installation_6',['Installation',['../index.html#autotoc_md1',1,'']]],
+  ['integration_7',['CMake Integration',['../index.html#autotoc_md16',1,'']]],
+  ['inverse_8',['Inverse',['../classfound_1_1Mat3.html#aee2ad12694bb3267ae9db48070fc56e3',1,'found::Mat3']]],
+  ['isunit_9',['IsUnit',['../classfound_1_1Quaternion.html#a1a51d83a4545c557238ac1a088d6bc29',1,'found::Quaternion']]],
+  ['iterativesphericaldistancedeterminationalgorithm_10',['iterativesphericaldistancedeterminationalgorithm',['../classfound_1_1IterativeSphericalDistanceDeterminationAlgorithm.html',1,'found::IterativeSphericalDistanceDeterminationAlgorithm'],['../classfound_1_1IterativeSphericalDistanceDeterminationAlgorithm.html#a9e3adfab31b529b0a3e78d0b89577092',1,'found::IterativeSphericalDistanceDeterminationAlgorithm::IterativeSphericalDistanceDeterminationAlgorithm()']]]
+];
---
+++ 45/merge/doxygen/search/functions_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pixelsize_0',['PixelSize',['../classfound_1_1Camera.html#a440dba521fd099e814b4c29c203bdf93',1,'found::Camera']]],
+  ['pow_1',['Pow',['../classfound_1_1IterativeSphericalDistanceDeterminationAlgorithm.html#a6c2b5b765b46ebda847ea706b015d623',1,'found::IterativeSphericalDistanceDeterminationAlgorithm']]]
+];
