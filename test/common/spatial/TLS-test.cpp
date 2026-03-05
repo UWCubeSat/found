60f77cf8a5b2390929fa6898a1e7234c0b703878
Senuka Liyanage <senukasl@outlook.com>
2026-03-04 23:08:09 -0800

    😐

--- test/common/spatial/TLS-test.cpp
+++ test/common/spatial/TLS-test.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include <Eigen/Core>
 
-#include "common/logging.hpp"
 #include "test/common/common.hpp"
 
 #include "src/common/spatial/attitude-utils.hpp"
@@ -10,50 +9,50 @@
 #define TLS_TOLERANCE DECIMAL(0.05)
 
 #define VECTOR3_EQUALS(vec1, vec2, tolerance) \
-    EXPECT_LT(abs(vec1.x() - vec2.x()), tolerance); \
-    EXPECT_LT(abs(vec1.y() - vec2.y()), tolerance); \
-    EXPECT_LT(abs(vec1.z() - vec2.z()), tolerance);
+    EXPECT_LT(abs(vec1(0) - vec2(0)), tolerance); \
+    EXPECT_LT(abs(vec1(1) - vec2(1)), tolerance); \
+    EXPECT_LT(abs(vec1(2) - vec2(2)), tolerance);
 
 #define VECTOR2_EQUALS(vec1, vec2, tolerance) \
-    EXPECT_LT(abs(vec1.x() - vec2.x()), tolerance); \
-    EXPECT_LT(abs(vec1.y() - vec2.y()), tolerance);
+    EXPECT_LT(abs(vec1(0) - vec2(0)), tolerance); \
+    EXPECT_LT(abs(vec1(1) - vec2(1)), tolerance);
 
 
 TEST(TLSTest, vec3Test) {
-    Eigen::Matrix<decimal,6,4> data {
-        {DECIMAL(1),DECIMAL(1),DECIMAL(1),DECIMAL(3)},
-        {DECIMAL(1),DECIMAL(2),DECIMAL(3),DECIMAL(6)},
-        {DECIMAL(0),DECIMAL(0),DECIMAL(0),DECIMAL(0)},
-        {DECIMAL(-1),DECIMAL(-1),DECIMAL(-1),DECIMAL(-3)},
-        {DECIMAL(-1),DECIMAL(3),DECIMAL(0),DECIMAL(2)},
-        {DECIMAL(4329), DECIMAL(-4211), DECIMAL(0), DECIMAL(118)}
+    Eigen::MatrixXd data {
+        {1.0,1.0,1.0,3.0},
+        {1.0,2.0,3.0,6.0},
+        {0.0,0.0,0.0,0.0},
+        {-1.0,-1.0,-1.0,-3.0},
+        {-1.0,3.0,0.0,2.0},
+        {4329.0, -4211.0, 0.0, 118.0}
     };
-    Eigen::Vector3d expected(DECIMAL(1),DECIMAL(1),DECIMAL(1));
+    Eigen::Vector3d expected(1,1,1);
     Eigen::Vector3d actual = found::TLS(data);
     VECTOR3_EQUALS(actual, expected, DEFAULT_TOLERANCE);
 }
 
 TEST(TLSTest, vec2Test) {
-    Eigen::Matrix<decimal,4,3> data {
-        {0.239794,0,0.719382},
-        {1,2,5},
-        {0,0,0},
-        {-1,5,2}
+    Eigen::MatrixXd data {
+        {0.239794,0.0,0.719382},
+        {1.0,2.0,5.0},
+        {0.0,0.0,0.0},
+        {-1.0,5.0,2.0}
     };
-    Eigen::Vector<decimal, 2> expected(3,1);
-    Eigen::Vector<decimal, 2> actual = found::TLS(data);
+    Eigen::Vector2d expected(3,1);
+    Eigen::VectorXd actual = found::TLS(data);
     VECTOR2_EQUALS(actual, expected, DEFAULT_TOLERANCE);
 }
 
 TEST(TLSTest, vec2PermutatedTest) {
-    Eigen::Matrix<decimal,4,3> data {
-        {0.239793,0,0.719383},
-        {1.02,2,5.002},
+    Eigen::MatrixXd data {
+        {0.239793,0.0,0.719383},
+        {1.02,2.0,5.002},
         {0.001,0.0004,0.005},
-        {-1.001,5,2}
+        {-1.001,5.0,2.0}
     };
-    Eigen::Vector<decimal, 2> expected(3,1);
-    Eigen::Vector<decimal, 2> actual = found::TLS(data);
+    Eigen::Vector2d expected(3,1);
+    Eigen::VectorXd actual = found::TLS(data);
     VECTOR2_EQUALS(actual, expected, TLS_TOLERANCE);
 }
 
